c02f5dee60b1deca0cda7476fd761b4683856ea5
hexbreak <theownerproject@gmail.com>
2021-01-21 19:54:07 +0000

    Updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -32,6 +32,7 @@ export function Home() {
 		fetch("https://swapi.dev/api/people/")
 			.then(function(response) {
 				if (!response.ok) {
+					// if the response is good, it will avoid showing the error. If the response is NOT okay it will show up.
 					throw Error(response.statusText);
 				}
 				// Read the response as json.
@@ -41,13 +42,16 @@ export function Home() {
 				// Do stuff with the JSON
 				setCharacters(responseAsJson.results);
 				// because we are fetching information on "people" from SW. We are using the setState as SetCharacters
-				/* We need an array with characters inside the object. The specific properties value to take only the array for each character, like the url for /1/ is Luke Skywalker we need his info. 
-                    so we will use the .notation inside the paranthesis */
+				/* We need an array with characters from a specific object. The specific properties value to take only the array for each character, like the url for /1/ is Luke Skywalker we need his info. So we will use the .notation inside the paranthesis after _ setCharacters(responseAsJson); _ with _ setCharacters(responseAsJson.results); _ to pull the object we need */
+				// How can we test that the information we require works? use _ console.log(characters); _ results should be listing characters names from SW by order.
+				// let's say we want to pull something specific from the array/object? _ setState(responseAsJson.results.index.eye_color); _ or very specific like luke skywalker _ setState(responseAsJson.results.index[1].eye_color); _ and we will get the only the eye color of luke skywalker instead of everyone from the code before.
 			})
 			.catch(function(error) {
 				console.log("Looks like there was a problem: \n", error);
 			});
 	}, []);
+
+	// we will now copy the entire useEffect for the thePlanets & theStarships.
 
 	return (
 		<div className="container align-items-center">
