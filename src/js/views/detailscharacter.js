e4b302fcce7542ed61553b3de0294fe669c57564
hexbreak <theownerproject@gmail.com>
2021-02-01 18:33:08 +0000

    bootstrap fix

--- src/js/views/detailscharacter.js
+++ src/js/views/detailscharacter.js
@@ -8,22 +8,23 @@ export const DetailsCharacter = props => {
 	const { store, actions } = useContext(Context);
 	let { theid } = useParams();
 	const singleCharacter = store.theCharacters.find(element => element.name === theid);
-	const { name, birth_year, height, gender } = singleCharacter;
-	console.log(props.test);
+	const { name, birth_year, height, gender, mass } = singleCharacter;
+	console.log(singleCharacter);
 
 	return (
 		<div className="container-fluid d-flex justify-content-center">
 			<div className="card mb-3 bg-dark" style={{ maxWidth: "2000px" }}>
 				<div className="row g-0 text-white">
-					<div className="col-8">
+					<div className="col-9">
 						<img src={lukeImage} alt="Character Image" />
 					</div>
-					<div className="col-4">
+					<div className="col-3">
 						<div className="card-body">
 							<div className="card-title">Name: {name}</div>
-							<div className="card-text">Birth Dte goes here!</div>
-							<div className="card-text">Class goes here!</div>
-							<div className="card-text">More miscellaneous goes here!</div>
+							<div className="card-text">Birth Year: {birth_year}</div>
+							<div className="card-text">Gender: {gender}</div>
+							<div className="card-text">Height: {height}</div>
+							<div className="card-text">Mass: {mass}</div>
 							<div className="card-text">
 								<div className="text-muted">Go Back Button will go here!</div>
 							</div>
